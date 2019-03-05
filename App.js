10a4444198777cabbcc74bfe1cfc23fa28d6f645
Tsion Adefres <tadefres@gmail.com>
2019-03-04 23:04:37 -0800

    main app component

--- App.js
+++ App.js
@@ -13,7 +13,6 @@ import React, { Component } from 'react';
 import Main from './app/components/Main';
 
 type Props = {};
-
 export default class App extends Component<Props>{
   render() {
     return (
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /**
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
