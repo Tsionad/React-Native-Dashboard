5dbd7617cb16d8760a4c08faba57733e9913b5f8
Tsion Adefres <tadefres@gmail.com>
2019-03-04 20:38:42 -0800

    Restructred app layout

---
+++ app/components/Main.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity,
+} from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View><Text>Nothing to display...</Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
